660a953bd955e2aecb3435f7fbf5d1a363e07bee
Zhalgas Baibatyr <mail@zhalgas.com>
2023-02-09 18:48:13 +0600

    Updates

--- Tests/BaseXITests/BaseXITests.swift
+++ Tests/BaseXITests/BaseXITests.swift
@@ -1,12 +1,4 @@
 import XCTest
 @testable import BaseXI
 
-final class BaseXITests: XCTestCase {
-    
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-//        XCTAssertEqual(BaseXI().text, "Hello, World!")
-    }
-}
+final class BaseXITests: XCTestCase {}
